7c350ced346d03962934ff70184adb2e6e6ec831
fs <fs@imagine-orange.com>
2018-05-01 19:08:44 +0200

    test ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, QueryList, ViewChild, ViewChildren} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./_app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+    @ViewChild('grid1') grid1;
+    @ViewChildren('items') aItems;
+    lastItemRect;
+    Math = Math;
+    random = 0;
+    items = [
+        ['1/1', '2/1', '3/1'],
+        ['2/1', '2/2', '2/3'],
+        ['1/1', '2/1', '2/2', '3/2', '5/1'],
+        ['1/1', '1/2', '2/1', '2/2', '3/1', '3/2'],
+        ['1/1', '1/2', '1/3', '2/1', '2/2', '2/3', '3/1', '3/2', '3/3']
+    ];
+
+    constructor() {
+        setInterval(() => {
+
+            // change items
+            this.random =
+                Math.floor(Math.random() * this.items.length);
+
+            // give a tick to allow repositioning, then get pos of last item
+            setTimeout( () =>
+                this.lastItemRect = this.aItems.last.nativeElement.getBoundingClientRect(),
+            0);
+
+        }, 2000);
+    }
+
+    ngAfterViewInit() {
+        console.log(this.grid1.nativeElement.getBoundingClientRect());
+        console.log(this.aItems);
+
+    }
 }
